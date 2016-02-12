f11f0caeadd261a04cb809088abedccd1efb2798
Andrew Jacobson <andyjacuw@gmail.com>
2016-02-11 21:42:00 -0800

    Add Stat model

---
+++ models/index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config.json')[env];
+var db = {};
+
+var sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password, {
+    dialect: 'postgres',
+    logging: false
+  }
+);
+
+fs.readdirSync(__dirname).filter(function(file) {
+  return (file[0] !== '.') && (file !== 'index.js');
+}).forEach(function(file) {
+  var model = sequelize.import(path.join(__dirname, file));
+  db[model.name] = model;
+});
+
+Object.keys(db).forEach(function(modelName) {
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+//sequelize.sync({force: true});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
---
+++ models/stat.js
@@ -0,0 +1,88 @@
+module.exports = function(sequelize, DataTypes) {
+  var Stat = sequelize.define('Stat', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    location_id: {
+      type: DataTypes.INTEGER
+    },
+    location: {
+      type: DataTypes.STRING
+    },
+    location_name: {
+      type: DataTypes.STRING
+    },
+    year: {
+      type: DataTypes.INTEGER
+    },
+    age_group_id: {
+      type: DataTypes.INTEGER
+    },
+    age_group: {
+      type: DataTypes.STRING
+    },
+    age_start: {
+      type: DataTypes.INTEGER
+    },
+    age_end: {
+      type: DataTypes.INTEGER
+    },
+    sex_id: {
+      type: DataTypes.INTEGER
+    },
+    sex: {
+      type: DataTypes.STRING
+    },
+    unit: {
+      type: DataTypes.STRING
+    },
+    metric: {
+      type: DataTypes.STRING
+    },
+    measure: {
+      type: DataTypes.STRING
+    },
+    mean: {
+      type: DataTypes.DECIMAL
+    },
+    lower: {
+      type: DataTypes.DECIMAL
+    },
+    upper: {
+      type: DataTypes.DECIMAL
+    }
+  }, {
+    classMethods: {
+      getAll: function(cb) {
+        this.findAll({
+          attributes: ['id', 'location']
+        }).then(function(stats) {
+          console.log(stats.length);
+          cb(null, stats);
+          return null;
+        }).error(function(err) {
+          cb(err);
+          return null;
+        });
+      },
+
+      getAllByLocation: function(locationId, cb) {
+        this.findAll({
+          where: {
+            location_id: locationId
+          }
+        }).then(function(stats) {
+          cb(null, stats);
+          return null;
+        }).catch(function(err) {
+          cb(err);
+          return null;
+        });
+      }
+    }
+  });
+
+  return Stat;
+};

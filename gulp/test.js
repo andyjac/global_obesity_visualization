b84399bbdc58e92ca2f0f04fd36efd139ce406fb
Andrew Jacobson <andyjacuw@gmail.com>
2016-02-12 00:09:27 -0800

    Remove commented code, exit tests on end

--- gulp/test.js
+++ gulp/test.js
@@ -2,16 +2,6 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var paths = require('./paths');
 
-require('./lint');
-
-// gulp.task('test:lib', ['lint:lib'], function() {
-//   return test(paths.test.lib);
-// });
-
-// gulp.task('test:server', ['lint:server'], function() {
-//   return test(paths.test.server);
-// });
-
 gulp.task('test', function() {
   return test(paths.test);
 });
@@ -24,5 +14,7 @@ function test(paths) {
     .once('error', function(err){
       console.log(err);
       process.exit(1);
+    }).once('end', function() {
+      process.exit(0);
     });
 }

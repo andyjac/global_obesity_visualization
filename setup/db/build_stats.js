f2dbbd3ed85c9cf0480861552d2f17b7be5836a1
Andrew Jacobson <andyjacuw@gmail.com>
2016-02-13 02:32:08 -0800

    Only save used attrs to model

--- setup/db/build_stats.js
+++ setup/db/build_stats.js
@@ -1,17 +1,28 @@
 var _ = require('lodash');
 var helpers = require('../../lib/helpers');
 
+var keyIsUsed = {
+  'location_id': true,
+  'location_name': true,
+  'year': true,
+  'mean': true,
+  'sex': true
+};
+
 module.exports = function(input, cb) {
   var keys = helpers.parseKeys(input);
   var data = helpers.parseData(input);
 
-  var stats = _.map(data, function(dataString) {
+  var stats = _.filter(data, function(dataString) {
     var attrString = helpers.sanitizeString(dataString);
     var attrs = attrString.split(',');
 
     return _.reduce(attrs, function(stat, value, i) {
       var key = keys[i];
-      stat[key] = value.replace(/\|/g, ',');
+
+      if (keyIsUsed[key]) {
+        stat[key] = value.replace(/\|/g, ',');
+      }
 
       return stat;
     }, {});
